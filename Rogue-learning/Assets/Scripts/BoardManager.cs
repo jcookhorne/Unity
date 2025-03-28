b0af6cdd05e942a1013ec7c8bbd329a6c240a776
Joshua Cookhorne <jncookhorne@gmail.com>
2025-03-27 20:11:24 -0400

    added basic player movements into the game and adding the character

--- Rogue-learning/Assets/Scripts/BoardManager.cs
+++ Rogue-learning/Assets/Scripts/BoardManager.cs
@@ -9,18 +9,23 @@ public class BoardManager : MonoBehaviour
     public int height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
+    private Grid m_Grid;
+    public PlayerController Player;
+    private CellData[,] m_BoardData;
 
     public class CellData
     {
         public bool Passable;
     }
-    private CellData[,] m_BoardData;
+
+  
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
-
+        m_Grid = GetComponentInChildren<Grid>();
         m_BoardData = new CellData[width, height];
 
         for (int y = 0; y < height; y++)
@@ -42,15 +47,28 @@ public class BoardManager : MonoBehaviour
                     m_BoardData[x, y].Passable = true;
     
                 }
-
-
-                   
+ 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
-        
+
+        Player.Spawn(this, new Vector2Int(1, 1));
         
     }
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if(cellIndex.x<0 || cellIndex.x >= width || cellIndex.y <0 || cellIndex.y >= height)
+        {
+            return null;
+        }
+        return m_BoardData[cellIndex.x, cellIndex.y];
+    }
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+
 
     // Update is called once per frame
     void Update()
