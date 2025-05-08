f6f1cd5aabe29c3e6d4d8dc1cb5f27dc2f317eff
Joshua Cookhorne <jncookhorne@gmail.com>
2025-05-07 23:52:33 -0400

    updated movement and rotation on movement

--- Practice/Assets/Scripts/PlayerController.cs
+++ Practice/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     PlayerInput playerInput;
     CharacterController characterController;
+
     Vector2 currentMovementInput;
     Vector3 currentMovement;
+    Vector3 currentRunMovement;
     bool isMovementPressed;
-    public float speed = 5f;
+    float rotationFactorPerFrame = 15.0f;
+    bool isRunPressed;
 
     void Awake()
     {
@@ -17,22 +20,76 @@ public class PlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         playerInput = new PlayerInput();
 
-        playerInput.CharacterControls.Move.started += context =>
-        { 
-           currentMovementInput = context.ReadValue<Vector2>();
-           currentMovement.x = currentMovementInput.x;
-           currentMovement.z = currentMovementInput.y;
-           isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
-
-        };
-
+        playerInput.CharacterControls.Move.started += onMovementInput;
+        playerInput.CharacterControls.Move.canceled += onMovementInput;
+        // good for controllers for responsivness
+        playerInput.CharacterControls.Move.performed += onMovementInput;
+        playerInput.CharacterControls.Move.started += onRun;
+        playerInput.CharacterControls.Move.canceled += onRun;
     }
-    
+
     // Update is called once per frame
-    void Update() { 
-    
+    void Update()
+    {
+        handleRotation();
+        handleAnimation();
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    void handleRotation()
+    {
+        Vector3 positionToLookAt;
+        // the change in position our character should point to
+        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.z;
+
+        // the current rotation of our character
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed)
+        {
+            // creates a new rotation based on where the plater is currentl pressing
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation =  Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+
+        }
+    }
+
+
+    void handleAnimation()
+    {
+        bool isWalking = animator.GetBool("Walk");
+        bool isIdle = animator.GetBool("Idle");
+
+        if (isMovementPressed && !isWalking)
+        {
+            animator.SetBool("isWalking", true);
+
+        }
+        else if (!isMovementPressed && isWalking)
+        {
+            animator.SetBool("isWalking", false);
+        }
 
     }
+
+    void onMovementInput(InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+
+    }
+    
+    void onRun (InputAction.CallbackContext context)
+    {
+        isRunPressed = context.ReadValueAsButton();
+    }
+
+
+
 
     private void OnEnable()
     {
